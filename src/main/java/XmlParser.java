0f49b3e0698f681c0fd74edec7359d87b7ece9ed
Jon Jakse <tripleplayj@gmail.com>
2019-11-26 21:15:21 -0600

    feat: more counting, total word map, etc.

--- src/main/java/XmlParser.java
+++ src/main/java/XmlParser.java
@@ -8,6 +8,7 @@ import java.text.ParseException;
 public class XmlParser {
     public static void main(String[] args) throws FileNotFoundException, XMLStreamException, ParseException {
         File resourcesDirectory = new File("src/main/resources/sms-20191110120028.xml");
-        XmlReader.parse(resourcesDirectory);
+        XmlReader reader = new XmlReader();
+        reader.parse(resourcesDirectory);
     }
 }
--- src/main/java/reader/XmlReader.java
+++ src/main/java/reader/XmlReader.java
@@ -1,27 +1,25 @@
 package reader;
 
+import org.joda.time.DateTime;
+
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.*;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-import static java.util.Map.Entry.comparingByKey;
 import static java.util.Map.Entry.comparingByValue;
 import static java.util.stream.Collectors.toMap;
 
 public class XmlReader {
 
-    public static void parse(File file) throws FileNotFoundException, XMLStreamException, ParseException {
+    public void parse(File file) throws FileNotFoundException, XMLStreamException, ParseException {
         XMLInputFactory factory = XMLInputFactory.newInstance();
         XMLEventReader eventReader = factory.createXMLEventReader(new FileReader(file));
 
@@ -31,32 +29,26 @@ public class XmlReader {
         Map<String, Integer> cToJWordMap = new HashMap<>();
         Map<String, Integer> jToCWordMap = new HashMap<>();
         Map<String, Integer> textsByDateMap = new HashMap<>();
+        Map<String, Integer> textsByTimeMap = new HashMap<>();
 
         while (eventReader.hasNext()) {
             XMLEvent event = eventReader.nextEvent();
-
             if (event.isStartElement()) {
                 StartElement element = (StartElement) event;
-
                 if (element.getName().toString().equalsIgnoreCase("sms")) {
                     String contactName = element.getAttributeByName(new QName("", "contact_name")).getValue();
                     String message = element.getAttributeByName(new QName("", "body")).getValue();
                     String type = element.getAttributeByName(new QName("", "type")).getValue();
-                    String date = element.getAttributeByName(new QName("", "readable_date")).getValue();
+                    String dateTimestamp = element.getAttributeByName(new QName("", "date")).getValue();
                     Map<String, Integer> currMap = new HashMap<String, Integer>();
-                    //TODO: readd appropriate contact name here
+                    //TODO: read appropriate contact name here
                     if (contactName.equals("")) {
-                        //TODO: I was too tired to parse dates here, but wanted to see if it would work
-                        String[] splitDate = date.split(" ");
-                            String stupidParsedDate = splitDate[0] + splitDate[1] + splitDate[2];
-                            if(textsByDateMap.get(stupidParsedDate) == null) {
-                                textsByDateMap.put(stupidParsedDate, 1);
-                            }
-                            else {
-                                int oldValue = textsByDateMap.get(stupidParsedDate);
-                                oldValue++;
-                                textsByDateMap.put(stupidParsedDate, oldValue);
-                            }
+                        DateTime dt = new DateTime( Long.parseLong( dateTimestamp ) ) ;
+                        String dateTextSent = dt.getMonthOfYear() + "/" + dt.getDayOfMonth() + "/" + dt.getYear();
+                        insertOrIncMapCount(textsByDateMap, dateTextSent);
+                        String timeTextSent = dt.getHourOfDay() + ":";
+                        timeTextSent += dt.getMinuteOfHour() < 10 ? "0" + dt.getMinuteOfHour() :   dt.getMinuteOfHour();
+                        insertOrIncMapCount(textsByTimeMap, timeTextSent);
 
                         if(type.equals("1")) {
                             cToJMsgCount++;
@@ -85,23 +77,42 @@ public class XmlReader {
         System.out.println("J to C " + jToCMsgCount);
         System.out.println("Total texts " + (cToJMsgCount + jToCMsgCount));
 
-        Map<String, Integer> cToJWordMapSorted = cToJWordMap
-                                                .entrySet()
-                                                .stream()
-                                                .sorted(Collections.reverseOrder(comparingByValue())).collect(toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2, LinkedHashMap::new));
+        Map<String, Integer> cToJWordMapSorted = sortMapDesc(cToJWordMap);
+        Map<String, Integer> jToCWordMapSorted = sortMapDesc(jToCWordMap);
+        Map<String, Integer> dateMapSorted = sortMapDesc(textsByDateMap);
+        Map<String, Integer> timeMapSorted = sortMapDesc(textsByTimeMap);
 
-        Map<String, Integer> jToCWordMapSorted = jToCWordMap
-                                                .entrySet()
-                                                .stream()
-                                                .sorted(Collections.reverseOrder(comparingByValue())).collect(toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2, LinkedHashMap::new));
+        //get total word count
+        Map<String, Integer> cWordMapCopy = new HashMap<>();
+        cWordMapCopy.putAll(cToJWordMap);
+        Map<String, Integer> jWordMapCopy = new HashMap<>();
+        jWordMapCopy.putAll(jToCWordMap);
+        cWordMapCopy.forEach((k, v) -> jWordMapCopy.merge(k, v, Integer::sum));
+        sortMapDesc(jWordMapCopy);
 
-        Map<String, Integer> dateMapSorted = textsByDateMap
+        cToJWordMap.entrySet().forEach(entry->{
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
+
+        System.out.println();
+    }
+
+    private void insertOrIncMapCount( Map<String, Integer> map, String key) {
+        if (map.get(key) == null) {
+            map.put(key, 1);
+        }
+        else {
+            int oldValue = map.get(key);
+            oldValue++;
+            map.put(key, oldValue);
+        }
+    }
+
+    private Map<String, Integer> sortMapDesc(Map<String, Integer> map) {
+        return  map
                 .entrySet()
                 .stream()
                 .sorted(Collections.reverseOrder(comparingByValue())).collect(toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2, LinkedHashMap::new));
 
-        System.out.println();
-
-        System.out.println();
     }
 }
